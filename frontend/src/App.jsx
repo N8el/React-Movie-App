50c6d6cd8cde68a0958a3b51d7bab24ae1807f36
Noel <147927194+N8el@users.noreply.github.com>
2025-07-30 17:38:18 +0100

    Fix React Router basename, in vite.config.js and app.jsx for GitHub Pages deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <NavBar />
       <main className="main-content">
         <Routes>
-          <Route path="/Home" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
