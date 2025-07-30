b1e1a4ddcf834e13c608730b3b004558bafcefdd
Noel <147927194+N8el@users.noreply.github.com>
2025-07-30 18:10:10 +0100

    Fix: Import Navigate in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./css/App.css";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
